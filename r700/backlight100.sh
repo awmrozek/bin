a3b961fa6949897d19fb9bf3b6a891c4a29d649e
Adam Mrozek <dat14amr@student.lu.se>
2020-06-29 01:53:34 +0200

    add r700 specific scripts

---
+++ r700/backlight100.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+su -c "echo 12630405 > /sys/class/backlight/intel_backlight/brightness"
---
+++ r700/backlight40.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+su -c "echo 2500000 > /sys/class/backlight/intel_backlight/brightness"
